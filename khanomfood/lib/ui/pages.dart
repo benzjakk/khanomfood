a874f0aa283ae615fdbe932095853de5646f1bd8
Tanabodee Sangprajong <deempooh@gmail.com>
2020-07-28 21:43:19 +0700

    Update pages.dart

--- khanomfood/lib/ui/pages.dart
+++ khanomfood/lib/ui/pages.dart
@@ -1,1 +1,5 @@
 export './pages/home_page.dart';
+export './pages/kitchen_page.dart';
+export './pages/notification_page.dart';
+export './pages/setting_page.dart';
+export './pages/storefront_page.dart';
