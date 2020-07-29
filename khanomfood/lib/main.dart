e4288a5c6dc239b99ef4c26392efc8d503743c7c
Tanabodee Sangprajong <deempooh@gmail.com>
2020-07-29 19:16:13 +0700

    test commit

--- khanomfood/lib/main.dart
+++ khanomfood/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Khanom Food',
+      title: 'Khanom Food test commit',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
