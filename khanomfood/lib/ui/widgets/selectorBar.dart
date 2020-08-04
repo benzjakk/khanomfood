cfd4ea64b3eadf6bae5cbb240396fc2d6b0c8258
benzjakk <benzsport357@gmail.com>
2020-08-04 20:20:50 +0700

    Update selectorBar.dart
    made it dynamic

--- khanomfood/lib/ui/widgets/selectorBar.dart
+++ khanomfood/lib/ui/widgets/selectorBar.dart
@@ -7,7 +7,7 @@ class SelectorBar {
   PageController pageController = PageController();
   void UpdatePageController(int index) {
     pageController.animateToPage(index,
-        duration: Duration(milliseconds: 200), curve: Curves.linear);
+        duration: Duration(milliseconds: 500), curve: Curves.linear);
   }
 
   void UpdateSelectedColors(List<Color> selectedColors) {
@@ -18,94 +18,36 @@ class SelectorBar {
     return pageController;
   }
 
+  List<Container> GetMenuList() {
+    int n = menuText.length;
+    List<Container> result = [];
+    for (int i = 0; i < n; i++) {
+      result.add(Container(
+        width: 80,
+        height: 20,
+        child: FlatButton(
+          onPressed: () {
+            UpdatePageController(i);
+          },
+          child: Text(
+            menuText[i],
+            style: TextStyle(
+                fontFamily: 'supermarket',
+                fontSize: 15,
+                color: selectedColors[i]),
+          ),
+        ),
+      ));
+    }
+    return result;
+  }
+
   Container DisplaySelectorBar() {
     return Container(
       color: Colors.white,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            width: 80,
-            height: 20,
-            child: FlatButton(
-              onPressed: () {
-                UpdatePageController(0);
-              },
-              child: Text(
-                menuText[0],
-                style: TextStyle(
-                    fontFamily: 'supermarket',
-                    fontSize: 15,
-                    color: selectedColors[0]),
-              ),
-            ),
-          ),
-          Container(
-            width: 80,
-            height: 20,
-            child: FlatButton(
-              onPressed: () {
-                UpdatePageController(1);
-              },
-              child: Text(
-                menuText[1],
-                style: TextStyle(
-                    fontFamily: 'supermarket',
-                    fontSize: 15,
-                    color: selectedColors[1]),
-              ),
-            ),
-          ),
-          Container(
-            width: 80,
-            height: 20,
-            child: FlatButton(
-              onPressed: () {
-                UpdatePageController(2);
-              },
-              child: Text(
-                menuText[2],
-                style: TextStyle(
-                    fontFamily: 'supermarket',
-                    fontSize: 15,
-                    color: selectedColors[2]),
-              ),
-            ),
-          ),
-          Container(
-            width: 80,
-            height: 20,
-            child: FlatButton(
-              onPressed: () {
-                UpdatePageController(3);
-              },
-              child: Text(
-                menuText[3],
-                style: TextStyle(
-                    fontFamily: 'supermarket',
-                    fontSize: 15,
-                    color: selectedColors[3]),
-              ),
-            ),
-          ),
-          Container(
-            width: 80,
-            height: 20,
-            child: FlatButton(
-              onPressed: () {
-                UpdatePageController(4);
-              },
-              child: Text(
-                menuText[4],
-                style: TextStyle(
-                    fontFamily: 'supermarket',
-                    fontSize: 15,
-                    color: selectedColors[4]),
-              ),
-            ),
-          ),
-        ],
-      ),
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: GetMenuList()),
     );
   }
 }
